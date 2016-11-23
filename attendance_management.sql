9a4e8446c424cf48b037710ba2f9964175a0ada6
QiuQiang <1131402453@qq.com>
2016-11-23 15:58:06 +0800

    数据库
    +用户
    +请假单
    +部门

--- attendance_management.sql
+++ attendance_management.sql
@@ -10,10 +10,52 @@ Target Server Type    : MYSQL
 Target Server Version : 50714
 File Encoding         : 65001
 
-Date: 2016-11-21 19:38:00
+Date: 2016-11-23 15:57:08
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for department
+-- ----------------------------
+DROP TABLE IF EXISTS `department`;
+CREATE TABLE `department` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `department_name` varchar(255) DEFAULT NULL,
+  `department_manager` int(11) DEFAULT NULL,
+  `vice_general_manager` int(11) DEFAULT NULL,
+  `general_manager` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of department
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for leavebill
+-- ----------------------------
+DROP TABLE IF EXISTS `leavebill`;
+CREATE TABLE `leavebill` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `application_time` datetime DEFAULT NULL,
+  `leave_date` date DEFAULT NULL,
+  `num_of_days` int(11) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `describe` varchar(255) DEFAULT NULL,
+  `state` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of leavebill
+-- ----------------------------
+INSERT INTO `leavebill` VALUES ('1', '2016-11-22 16:30:09', '2016-11-23', '1', '1', 'aaaaaaaa', '1');
+INSERT INTO `leavebill` VALUES ('2', '2016-11-09 16:30:42', '2016-11-12', '1', '1', 'bbbbbb', '1');
+INSERT INTO `leavebill` VALUES ('3', '2016-11-22 16:31:02', '2016-11-24', '2', '1', 'qqqqqqq', '1');
+INSERT INTO `leavebill` VALUES ('4', '2016-11-02 16:31:21', '2016-11-08', '3', '1', 'wwwwwwww', '1');
+INSERT INTO `leavebill` VALUES ('5', '2016-11-18 16:31:43', '2016-11-29', '1', '1', 'rrrrrrrrr', '1');
+INSERT INTO `leavebill` VALUES ('6', null, null, null, null, null, null);
 
 -- ----------------------------
 -- Table structure for user
@@ -24,12 +66,46 @@ CREATE TABLE `user` (
   `user_name` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
+  `sex` varchar(255) DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `entry_date` date DEFAULT NULL,
+  `position` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', '123', '123', 'qqq', '123456@789.com');
-INSERT INTO `user` VALUES ('2', 'abc', 'abc', 'aaa', 'abc@def.com');
+INSERT INTO `user` VALUES ('1', '123', '123', 'qqq', '男', '2000-01-01', '12345678', '2016-01-01', '1', '123456@789.com');
+INSERT INTO `user` VALUES ('2', 'abc', 'abc', 'aaa', null, null, null, null, null, 'abc@def.com');
+INSERT INTO `user` VALUES ('3', '456', '456', 'qqq', '男', '2001-06-06', '45612379', '2016-02-22', '1', null);
+
+-- ----------------------------
+-- Table structure for user_department
+-- ----------------------------
+DROP TABLE IF EXISTS `user_department`;
+CREATE TABLE `user_department` (
+  `employee_id` int(11) NOT NULL,
+  `department_id` int(11) NOT NULL,
+  PRIMARY KEY (`employee_id`,`department_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user_department
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for user_leave
+-- ----------------------------
+DROP TABLE IF EXISTS `user_leave`;
+CREATE TABLE `user_leave` (
+  `user_id` int(11) NOT NULL,
+  `leave_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`,`leave_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user_leave
+-- ----------------------------
